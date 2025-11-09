57ecc59f51e5069271023227cd185a5e94434c8c
oGhartey <ondghartey@gmail.com>
2025-11-09 18:41:20 +0000

    advanced typescript

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,79 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getToWork(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+}
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+function workDirectorTasks(): string {
+  return "Getting to director tasks";
+}
+
+function workTeacherTasks(): string {
+  return "Getting to work";
+}
+
+function isDirector(employee: Teacher | Director): employee is Director {
+  return (employee as Director).numberOfReports !== undefined;
+}
+
+function executeWork(employee: Teacher | Director): string {
+  if (isDirector(employee)) {
+    return workDirectorTasks();
+  } else {
+    return workTeacherTasks();
+  }
+}
